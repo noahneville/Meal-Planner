33082879af5958bf9d5a2262d2bfd096a7a6746b
Christian Carnevale <cncarnevale@gmail.com>
2022-04-07 10:21:45 -0400

    api pull from spoontacular with array of data created appropriately for other code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-var one = 1;
+function getWineParingAPI (foodId) {
+  // fetch request gets a list of all the repos for the node.js organization
+var requestUrl = 'https://api.spoonacular.com/food/wine/pairing?food=steak&apiKey=d2130512b2d04807b0aefd43f5d70f29';
+// change this to: var requestUrl = 'https://api.spoonacular.com/food/wine/pairing?food=' + foodId + 
+//  '&apiKey=d2130512b2d04807b0aefd43f5d70f29'; 
+// and then test it of course
+  console.log(requestUrl);
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    console.log(data.pairedWines);
+    var suggestedWine = data;
+    console.log(suggestedWine);
+    return suggestedWine;
+
+  })
+}
+
+getWineParingAPI();
+
+var fetchButton = document.getElementById("fetch3");
+fetchButton.addEventListener('click', getWineParingAPI); 

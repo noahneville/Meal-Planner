0ad27eeef62d149a6718d653ba3db4e9bca1df8b
Noah Brunner <noahneville@gmail.com>
2022-04-07 17:39:08 -0400

    integrated api call functions with display functiosn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,230 +1,213 @@
-function getWineParingAPI (foodObject) {
+function getWineParingAPI(foodObject) {
   var foodId = foodObject.protein;
-var requestUrl = 'https://api.spoonacular.com/food/wine/pairing?food=' + foodId + '&apiKey=9ba115ccffc8427f9c17e1ce8f1010b0'; 
-fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    console.log(data.pairedWines);
-    var suggestedWine = data;
-    console.log(suggestedWine);
-    return suggestedWine;
-  })
+  var requestUrl =
+    "https://api.spoonacular.com/food/wine/pairing?food=" +
+    foodId +
+    "&apiKey=9ba115ccffc8427f9c17e1ce8f1010b0";
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log(data.pairedWines);
+      var suggestedWine = data;
+      console.log(suggestedWine);
+      displayWines(suggestedWine);
+      // return suggestedWine;
+    });
 }
 
-function getMealAPI (recipeID) {
-    var cuisineID = recipeID.cuisine;
-    var proteinID = recipeID.protein;
-    var restrictionID = recipeID.restriction;
-    let apiURL = "https://api.edamam.com/api/recipes/v2?type=public&q=" + proteinID + "&app_id=d1e52e14&app_key=cd5289aff6cb193787a2baa6b251ec23&health=" + restrictionID+ "&cuisineType="  + cuisineID 
-  
-    console.log(apiURL);
-    fetch(apiURL)
+function getMealAPI(recipeID) {
+  var cuisineID = recipeID.cuisine;
+  var proteinID = recipeID.protein;
+  var restrictionID = recipeID.restriction;
+  let apiURL =
+    "https://api.edamam.com/api/recipes/v2?type=public&q=" +
+    proteinID +
+    "&app_id=d1e52e14&app_key=cd5289aff6cb193787a2baa6b251ec23&health=" +
+    restrictionID +
+    "&cuisineType=" +
+    cuisineID;
+
+  console.log(apiURL);
+  fetch(apiURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data.hits);
-    var recipe = data;
-    console.log(recipe);
-      return recipe;
-  
-    })
-  }
-
-
+      var recipe = data;
+      console.log(recipe);
+      displayRecipes(recipe);
+      // return recipe;
+    });
+}
 
 var fetchButton = document.getElementById("fetch3");
-fetchButton.addEventListener('click', getMealAPI); 
-fetchButton.addEventListener('click', getWineParingAPI); 
+fetchButton.addEventListener("click", getMealAPI);
+fetchButton.addEventListener("click", getWineParingAPI);
 
+async function displayRecipes(Obj) {
+  // var tempURL = await fetch(apiURL);
+  // var Obj = await tempURL.json();
+  console.log(Obj);
+  const recipeSubtitle = document.querySelector(".recipe-subtitle");
+  recipeSubtitle.textContent = "Recipes Found:";
 
-// temporarily using this API call already written out so I can test my function without needing to wait for others to finish functions that do the actual api call
-// let apiURL = "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=d1e52e14&app_key=cd5289aff6cb193787a2baa6b251ec23&health=egg-free&cuisineType=American"
+  for (var i = 0; i < 3; i++) {
+    var foodName = Obj.hits[i].recipe.label;
+    var imgURL = Obj.hits[i].recipe.images.SMALL.url;
+    var servings = Obj.hits[i].recipe.yield;
+    var totalCal = Obj.hits[i].recipe.calories;
+    var calories = Math.round(totalCal / servings);
+    var fat = Obj.hits[i].recipe.digest[0].total.toFixed(1);
+    var carbs = Obj.hits[i].recipe.digest[1].total.toFixed(1);
+    var protein = Obj.hits[i].recipe.digest[2].total.toFixed(1);
+    var ingredients = Obj.hits[i].recipe.ingredients;
 
-async function displayRecipes () {
-    var tempURL = await fetch(apiURL);
-    var Obj = await tempURL.json();
-    console.log(Obj);
-    const recipeSubtitle = document.querySelector(".recipe-subtitle");
-    recipeSubtitle.textContent = "Recipes Found:";
+    const recipeGridEl = document.querySelector("#recipe-grid");
 
-    for (var i = 0; i < 3; i++){
-        var foodName = Obj.hits[i].recipe.label;
-        var imgURL = Obj.hits[i].recipe.images.SMALL.url;
-        var servings = Obj.hits[i].recipe.yield;
-        var totalCal = Obj.hits[i].recipe.calories;
-        var calories = Math.round(totalCal / servings);
-        var fat = Obj.hits[i].recipe.digest[0].total.toFixed(1);
-        var carbs = Obj.hits[i].recipe.digest[1].total.toFixed(1);
-        var protein = Obj.hits[i].recipe.digest[2].total.toFixed(1);
-        var ingredients = Obj.hits[i].recipe.ingredients;
+    const recipeContainerEl = document.createElement("div");
+    recipeContainerEl.classList.add("recipe-container", "col");
 
-        const recipeGridEl = document.querySelector("#recipe-grid");
+    var recipeCardEl = document.createElement("div");
+    recipeCardEl.setAttribute("class", "card");
 
-        const recipeContainerEl = document.createElement("div");
-        recipeContainerEl.classList.add("recipe-container", "col");
-        
-        var recipeCardEl = document.createElement("div");
-        recipeCardEl.setAttribute("class", "card");
+    var foodNameEl = document.createElement("div");
+    foodNameEl.textContent = foodName;
+    foodNameEl.classList.add("card-header", "text-center");
 
-        var foodNameEl = document.createElement("div");
-        foodNameEl.textContent = foodName;
-        foodNameEl.classList.add("card-header", "text-center");
+    var foodImgEl = document.createElement("img");
+    foodImgEl.setAttribute("src", imgURL);
+    foodImgEl.setAttribute("alt", "Picture of prepared recipe");
+    foodImgEl.classList.add("card-img-top");
 
-        var foodImgEl = document.createElement("img");
-        foodImgEl.setAttribute("src", imgURL);
-        foodImgEl.setAttribute("alt", "Picture of prepared recipe");
-        foodImgEl.classList.add("card-img-top");
+    var servingsEl = document.createElement("p");
+    servingsEl.textContent = servings + " Servings";
+    servingsEl.classList.add("card");
 
-        var servingsEl = document.createElement("p");
-        servingsEl.textContent = servings + " Servings";
-        servingsEl.classList.add("card");
+    var caloriesEl = document.createElement("p");
+    caloriesEl.textContent = calories + " Calories Per Serving";
+    caloriesEl.classList.add("card");
 
-        var caloriesEl = document.createElement("p");
-        caloriesEl.textContent = calories + " Calories Per Serving";
-        caloriesEl.classList.add("card")
+    var macrosEl = document.createElement("ul");
+    macrosEl.classList.add("card");
 
-        var macrosEl = document.createElement("ul");
-        macrosEl.classList.add("card");
+    var fatEl = document.createElement("li");
+    fatEl.textContent = "Fat - " + fat + "g";
+    var carbsEl = document.createElement("li");
+    carbsEl.textContent = "Carbs - " + carbs + "g";
+    var proteinEl = document.createElement("li");
+    proteinEl.textContent = "Protein - " + protein + "g";
 
-        var fatEl = document.createElement("li");
-        fatEl.textContent = "Fat - " + fat +"g";
-        var carbsEl = document.createElement("li");
-        carbsEl.textContent = "Carbs - " + carbs +"g";
-        var proteinEl = document.createElement("li");
-        proteinEl.textContent = "Protein - " + protein +"g";
+    macrosEl.appendChild(fatEl);
+    macrosEl.appendChild(carbsEl);
+    macrosEl.appendChild(proteinEl);
 
-        macrosEl.appendChild(fatEl);
-        macrosEl.appendChild(carbsEl);
-        macrosEl.appendChild(proteinEl);
-
-        const ingredientsListEl = document.createElement("ul");
-        ingredientsListEl.classList.add("card");
-        for (var x = 0; x < ingredients.length; x++) {
-            const ingredientsEl = document.createElement("li");
-            ingredientsEl.textContent = ingredients[x].text;
-            ingredientsListEl.appendChild(ingredientsEl);
-        }
-
-
-        recipeCardEl.appendChild(foodNameEl);
-        recipeCardEl.appendChild(foodImgEl);
-        recipeCardEl.appendChild(servingsEl);
-        recipeCardEl.appendChild(caloriesEl);
-        recipeCardEl.appendChild(macrosEl);
-        recipeCardEl.appendChild(ingredientsListEl);
-
-        recipeContainerEl.appendChild(recipeCardEl); 
-        recipeGridEl.appendChild(recipeContainerEl);
-    }
-}
-
-// let apiWineURL = "https://api.spoonacular.com/food/wine/pairing?food=chicken&apiKey=d2130512b2d04807b0aefd43f5d70f29"
-let apiWineURL = "https://api.spoonacular.com/food/wine/pairing?food=chicken&apiKey=144f5a3e0baa4bf4849131e69ddb39cf"
-
-async function displayWines () {
-    var tempWine = await fetch(apiWineURL);
-    console.log(tempWine);
-    var Obj = await tempWine.json();
-    console.log(Obj);
-
-    let wineList = Obj.pairedWines;
-    console.log(wineList);
-    
-    let wineText = Obj.pairingText;
-    
-    let wineImg = Obj.productMatches[0].imageUrl;
-    
-    let productName = Obj.productMatches[0].title;
-    
-    let productLink = Obj.productMatches[0].link;
-    
-
-    const wineSubtitle = document.querySelector(".wine-subtitle");
-    wineSubtitle.textContent = "Wine Recommendation:";
-
-    const wineGridEl = document.querySelector("#wine-grid");
-
-    const wineContainerEl = document.createElement("div");
-    wineContainerEl.classList.add("wine-container", "col");
-        
-    var wineCardEl = document.createElement("div");
-    wineCardEl.setAttribute("class", "card");
-
-    var wineImgEl = document.createElement("img");
-    wineImgEl.setAttribute("src", wineImg);
-    wineImgEl.setAttribute("alt", "Picture of wine label");
-    wineImgEl.classList.add("card-img");
-
-    var wineTextEl = document.createElement("p");
-    wineTextEl.textContent = wineText;
-    wineTextEl.classList.add("card");
-
-    var productNameEl = document.createElement("p");
-    productNameEl.textContent = productName;
-    productNameEl.classList.add("card");
-
-    var productLinkEl = document.createElement("button");
-    productLinkEl.textContent = "Click to purchase " + productName;
-    
-
-    const wineListEl = document.createElement("ul");
-    wineListEl.classList.add("card");
-    for (var j = 0; j < wineList.length; j++) {
-        const wineEl = document.createElement("li");
-        wineEl.textContent = wineList[j].text;
-        wineListEl.appendChild(wineEl);
+    const ingredientsListEl = document.createElement("ul");
+    ingredientsListEl.classList.add("card");
+    for (var x = 0; x < ingredients.length; x++) {
+      const ingredientsEl = document.createElement("li");
+      ingredientsEl.textContent = ingredients[x].text;
+      ingredientsListEl.appendChild(ingredientsEl);
     }
 
-    wineCardEl.appendChild(wineListEl);
-    wineCardEl.appendChild(wineTextEl);
-    wineCardEl.appendChild(wineImgEl);
-    wineCardEl.appendChild(productNameEl);
-    wineCardEl.appendChild(productLinkEl);
+    recipeCardEl.appendChild(foodNameEl);
+    recipeCardEl.appendChild(foodImgEl);
+    recipeCardEl.appendChild(servingsEl);
+    recipeCardEl.appendChild(caloriesEl);
+    recipeCardEl.appendChild(macrosEl);
+    recipeCardEl.appendChild(ingredientsListEl);
 
-    wineContainerEl.appendChild(wineCardEl);
-    wineGridEl.appendChild(wineContainerEl);
+    recipeContainerEl.appendChild(recipeCardEl);
+    recipeGridEl.appendChild(recipeContainerEl);
+  }
 }
 
+async function displayWines(Obj) {
+  // var tempWine = await fetch(apiWineURL);
+  // console.log(tempWine);
+  // var Obj = await tempWine.json();
+  console.log(Obj);
 
+  let wineList = Obj.pairedWines;
+  console.log(wineList);
 
-window.onload = function () {
-    displayWines();
-    
-};
+  let wineText = Obj.pairingText;
 
+  let wineImg = Obj.productMatches[0].imageUrl;
 
-var formSubmitHandler = function(event) {
+  let productName = Obj.productMatches[0].title;
+
+  let productLink = Obj.productMatches[0].link;
+
+  const wineSubtitle = document.querySelector(".wine-subtitle");
+  wineSubtitle.textContent = "Wine Recommendation:";
+
+  const wineGridEl = document.querySelector("#wine-grid");
+
+  const wineContainerEl = document.createElement("div");
+  wineContainerEl.classList.add("wine-container", "col");
+
+  var wineCardEl = document.createElement("div");
+  wineCardEl.setAttribute("class", "card");
+
+  var wineImgEl = document.createElement("img");
+  wineImgEl.setAttribute("src", wineImg);
+  wineImgEl.setAttribute("alt", "Picture of wine label");
+  wineImgEl.classList.add("card-img");
+
+  var wineTextEl = document.createElement("p");
+  wineTextEl.textContent = wineText;
+  wineTextEl.classList.add("card");
+
+  var productNameEl = document.createElement("p");
+  productNameEl.textContent = productName;
+  productNameEl.classList.add("card");
+
+  var productLinkEl = document.createElement("button");
+  productLinkEl.textContent = "Click to purchase " + productName;
+
+  const wineListEl = document.createElement("ul");
+  wineListEl.classList.add("card");
+  for (var j = 0; j < wineList.length; j++) {
+    const wineEl = document.createElement("li");
+    wineEl.textContent = wineList[j].text;
+    wineListEl.appendChild(wineEl);
+  }
+
+  wineCardEl.appendChild(wineListEl);
+  wineCardEl.appendChild(wineTextEl);
+  wineCardEl.appendChild(wineImgEl);
+  wineCardEl.appendChild(productNameEl);
+  wineCardEl.appendChild(productLinkEl);
+
+  wineContainerEl.appendChild(wineCardEl);
+  wineGridEl.appendChild(wineContainerEl);
+}
+
+var formSubmitHandler = function (event) {
   event.preventDefault();
-  var cuisineElement = document.querySelector("#cuisineSelection")
-  var proteinElement = document.querySelector("#proteinSelection")
-  var restrictionElement = document.querySelector("#restrictionSelection")
+  var cuisineElement = document.querySelector("#cuisineSelection");
+  var proteinElement = document.querySelector("#proteinSelection");
+  var restrictionElement = document.querySelector("#restrictionSelection");
   var cuisineChoice = cuisineElement.value;
   var proteinChoice = proteinElement.value;
   var restrictionChoice = restrictionElement.value;
-  console.log(cuisineChoice)
-  console.log(proteinChoice)
-  console.log(restrictionChoice)
+  console.log(cuisineChoice);
+  console.log(proteinChoice);
+  console.log(restrictionChoice);
   var choiceObject = {
-      cuisine: cuisineChoice,
-      protein: proteinChoice,
-      restriction:restrictionChoice,
-  }
- 
- getMealAPI(choiceObject);
-//  getWineParingAPI(choiceObject);
- return choiceObject;
-}
+    cuisine: cuisineChoice,
+    protein: proteinChoice,
+    restriction: restrictionChoice,
+  };
 
-var submitBtn = document.querySelector('#submitButton');
-submitBtn.addEventListener("click",formSubmitHandler);
+  getMealAPI(choiceObject);
+  getWineParingAPI(choiceObject);
+  return choiceObject;
+};
 
-// var cusine = document.getElementById("#cuisineSelection")
-// var ID = element.getAttribute("data")
-
-
-
-
+var submitBtn = document.querySelector("#submitButton");
+submitBtn.addEventListener("click", formSubmitHandler);

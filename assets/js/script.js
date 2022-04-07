2bf3c73aa1f0c2644f81dbc63be807fb0421f0f8
Noah Brunner <noahneville@gmail.com>
2022-04-06 23:25:19 -0400

    implemented boostrap row-columns to get cards to display in a row properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 
-const recipeContainerEl = document.querySelector("#recipe-container");
+
 // temporarily using this API call already written out so I can test my function without needing to wait for others to finish functions that do the actual api call
 let apiURL = "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=d1e52e14&app_key=cd5289aff6cb193787a2baa6b251ec23&health=egg-free&cuisineType=American"
 
@@ -16,28 +16,38 @@ async function displayRecipes () {
         var servings = Obj.hits[i].recipe.yield;
         var totalCal = Obj.hits[i].recipe.calories;
         var calories = Math.round(totalCal / servings);
-        var fat = Obj.hits[i].recipe.digest[0].total;
-        var carbs = Obj.hits[i].recipe.digest[1].total;
-        var protein = Obj.hits[i].recipe.digest[2].total;
+        var fat = Obj.hits[i].recipe.digest[0].total.toFixed(1);
+        var carbs = Obj.hits[i].recipe.digest[1].total.toFixed(1);
+        var protein = Obj.hits[i].recipe.digest[2].total.toFixed(1);
         var ingredients = Obj.hits[i].recipe.ingredients;
 
+        const recipeGridEl = document.querySelector("#recipe-grid");
+
+        const recipeContainerEl = document.createElement("div");
+        recipeContainerEl.classList.add("recipe-container", "col");
+        
         var recipeCardEl = document.createElement("div");
         recipeCardEl.setAttribute("class", "card");
 
-        var foodNameEl = document.createElement("span");
+        var foodNameEl = document.createElement("div");
         foodNameEl.textContent = foodName;
+        foodNameEl.classList.add("card-header", "text-center");
 
         var foodImgEl = document.createElement("img");
         foodImgEl.setAttribute("src", imgURL);
         foodImgEl.setAttribute("alt", "Picture of prepared recipe");
+        foodImgEl.classList.add("card-img-top");
 
         var servingsEl = document.createElement("p");
         servingsEl.textContent = servings + " Servings";
+        servingsEl.classList.add("card");
 
         var caloriesEl = document.createElement("p");
         caloriesEl.textContent = calories + " Calories Per Serving";
+        caloriesEl.classList.add("card")
 
         var macrosEl = document.createElement("ul");
+        macrosEl.classList.add("card");
 
         var fatEl = document.createElement("li");
         fatEl.textContent = "Fat - " + fat +"g";
@@ -51,6 +61,7 @@ async function displayRecipes () {
         macrosEl.appendChild(proteinEl);
 
         const ingredientsListEl = document.createElement("ul");
+        ingredientsListEl.classList.add("card");
         for (var x = 0; x < ingredients.length; x++) {
             const ingredientsEl = document.createElement("li");
             ingredientsEl.textContent = ingredients[x].text;
@@ -66,6 +77,7 @@ async function displayRecipes () {
         recipeCardEl.appendChild(ingredientsListEl);
 
         recipeContainerEl.appendChild(recipeCardEl); 
+        recipeGridEl.appendChild(recipeContainerEl);
     }
 }
 
